6ac9c7a43dacd8f3aee9aabc24bfe0ef4b5ebefa
sanjaysingh24 <sanjubora84@gmail.com>
2025-05-03 13:31:42 +0530

    update

--- fsmodules/fs.js
+++ fsmodules/fs.js
@@ -8,6 +8,31 @@ const fs = require('fs');
 
 // this is a synchronouse file read
 
-const data = fs.readFileSync("message.txt","utf-8");
-console.log(data);
-console.log("baba");// first it will print 
+// const data = fs.readFileSync("message.txt","utf-8");
+// console.log(data);
+// console.log("baba");// first it will print 
+
+
+// file write if not present then create a new file
+
+// fs.writeFile("new.txt","hmm bro ",(err)=>{
+//     if(err)throw err;
+//     console.log("text added successfully")
+// })
+
+// synchornouse
+//
+
+// const data = {
+//     name:"Sanjay",
+//     role:"Node.js"
+// }
+// fs.writeFileSync("user.json",JSON.stringify(data),(err)=>{
+//     if(err) throw err;
+//     console.log("JSON File Written")
+// })
+
+fs.appendFile("new.txt","\nNew Line added by sanjay ",(err)=>{
+    if(err) throw err;
+    console.log("Content update successfully")
+})
