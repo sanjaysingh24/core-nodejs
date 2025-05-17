ae284c4a7b40aec09bd8b4fde9c93ddb67a1e0b9
sanjaysingh24 <sanjubora84@gmail.com>
2025-05-17 20:19:28 +0530

    update

---
+++ errors/index.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+assert(2+2===4) //true
+assert(2+2===5) //false it send a error
+//type error  - comes when you try to do something that is not allowed in js
+//example let a = 12; a();// it show the type error
+//refrence error : when you  try to do with the variable which is not declared
+//range error: when you put a negative range line in array
+//syntax error: when you write a code which is not in the correct syntax
+//uncaught exception:error is not handled by try catch block
+//url error: when you wrote something wrong while using decodeurl and encodeurl
+//system  error: when system didnot found any file
+//user specified error: created by user custom error
+//assertion error: using a package when user supposed to be that thing are true but unfortunatley it will false
